51a67a802096051ebe9e9401264e57127ba556e5
chirag patel <chiragpatel08bce050@gmail.com>
2019-02-12 19:38:19 +0530

    api key change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './Components/search_bar'
 
-const API_key = "AIzaSyAE7kDG7ZZsBAaGxLPrmqbLw2yHVNB_tCU";
+const API_key = "";
 
 const App = () => {
   return (
